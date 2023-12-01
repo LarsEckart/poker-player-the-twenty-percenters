aa6077bd957ca43cbecbd8de1895f7ef97b27c30
Lars Eckart <lars.eckart@hey.com>
2023-12-01 15:44:29 +0200

    WIP add logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,17 @@
 package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    private static final Logger log = getLogger(Player.class);
+    static final String VERSION = "2-with-logging";
 
     public static int betRequest(JsonNode request) {
+        log.info("betRequest: {}", request);
         return 0;
     }
 
