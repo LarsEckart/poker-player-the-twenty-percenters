fb532116bd7e60f78462705bdb2a94f9d53811e5
Lars Eckart <lars.eckart@hey.com>
2023-12-01 16:07:46 +0200

    WIP more logging

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,7 +10,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class Player {
 
     private static final Logger log = getLogger(Player.class);
-    static final String VERSION = "3-with-extra-logs-and-all-in";
+    static final String VERSION = "4";
 
     public static int betRequest(JsonNode request) {
         log.info("betRequest: {}", request);
